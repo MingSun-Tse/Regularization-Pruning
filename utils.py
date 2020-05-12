693b72dc8666e91bde64b93835014c6c316f8324
Huan Wang <huanw@zju.edu.cn>
2020-05-12 15:53:09 -0400

    commented some in flops cal

--- utils.py
+++ utils.py
@@ -70,7 +70,7 @@ def get_n_flops(model=None, input_res=224, multiply_adds=True):
         batch_size = input[0].size(0) if input[0].dim() == 2 else 1
 
         weight_ops = self.weight.nelement() * (2 if multiply_adds else 1)
-        bias_ops = 0 self.bias.nelement()
+        bias_ops = self.bias.nelement()
 
         flops = batch_size * (weight_ops + bias_ops)
         list_linear.append(flops)
