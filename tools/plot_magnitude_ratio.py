7cfe0230aa2936ff5d945194adbc1b82495e378b
Huan Wang <huanw@zju.edu.cn>
2020-05-19 12:16:21 -0400

    added tools

---
+++ tools/plot_magnitude_ratio.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+import math
+
+'''Usgae:
+    python  this_file.py  <path to log.txt>
+'''
+f = sys.argv[1]
+mag_ratio = {}
+lines = [line.strip() for line in open(f)]
+n_line = len(lines)
+for i in range(n_line):
+    line = lines[i]
+    if "Mag ratio = " in line:
+        k = 1
+        while 'Update reg for layer' not in lines[i-k]:
+            k += 1
+        step = lines[i-k].split('Iter = ')[1].split('.')[0]
+        step = int(step)
+        layer_ix = lines[i-k].split(' ')[3].split('[')[1].split(']')[0]
+        mag_r = min(float(line.split('(')[1].split(')')[0]), 1e5)
+        if layer_ix in mag_ratio:
+            mag_ratio[layer_ix].append([step, mag_r])
+        else:
+            mag_ratio[layer_ix] = [[step, mag_r]]
+
+for k, v in mag_ratio.items():
+    fig, ax = plt.subplots()
+    v = np.array(v)
+    ax.plot(v[:,0], v[:,1])
+    ax.set_title(k)
+    ax.set_ylim([0, 1e5])
+    out_path = f.replace('log.txt', '%s.jpg' % k)
+    fig.savefig(out_path)
+    plt.close(fig)
